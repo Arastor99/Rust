ec781722b335012ec6771191731112044a6dc2d7
Alvaro Galo Tutor <60521799+Arastor99@users.noreply.github.com>
2022-12-30 04:11:48 +0100

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Tabla } from './components/Tabla'
+import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
     <Tabla/>
+    <Analytics />
+    </>
 )
