397f3ad1a06b28a3983f5d58851df8d1747f3b67
Alvaro Galo Tutor <60521799+Arastor99@users.noreply.github.com>
2023-06-14 15:21:23 +0200

    cambios vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Configuración de Google Analytics
+      analytics: {
+        id: 'G-QZ6Q78SEQK',
+        config: {
+          cookie_domain: 'auto',
+          cookie_expires: 63072000,
+          cookie_prefix: 'none',
+          cookie_update: true,
+          cookie_flags: '',
+          send_page_view: true,
+          allow_google_signals: true,
+          allow_ad_personalization_signals: true,
+        },
+        consentDefaults: {
+          analytics_storage: 'granted',
+          ad_storage: 'denied',
+          wait_for_update: 500,
+        },
+        persistentValues: {
+          currency: 'EUR',
+        },
+      },
+    }),
+  ],
+});
