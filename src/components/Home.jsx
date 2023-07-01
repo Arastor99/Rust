be706f66353902f68facc47583eefaa770c95a4e
Alvaro Galo Tutor <60521799+Arastor99@users.noreply.github.com>
2023-07-01 11:46:26 +0200

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -51,14 +51,6 @@ export const Home = () => {
       </div>
        */}
 
-      <Helmet>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8479674626279690"
-          crossorigin="anonymous"
-        ></script>
-      </Helmet>
-
       <Navbar />
       <div className="flex flex-col pt-24 bg-gray-800 md:flex-row overflow-x-hidden">
         <div className="w-full md:w-1/4 bg-gray-800 px-4 md:mx-4">
@@ -74,6 +66,14 @@ export const Home = () => {
           <div id="ezoic-pub-ad-placeholder-107"> </div>
         </div>
       </div>
+
+      <Helmet>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8479674626279690"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
     </>
   );
 };
