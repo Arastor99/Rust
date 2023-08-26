b6725a02f05a1d7dde487b5f730881b8b79b9cee
Arastor99 <1999alvarito@gmail.com>
2023-08-26 17:08:01 +0200

    fuente

--- src/components/Tabla.jsx
+++ src/components/Tabla.jsx
@@ -198,10 +198,10 @@ export const Tabla = () => {
               className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
               onClick={handleTiempo}
             >
-              <span className="text-xl">{t("messages.info4")}</span>
+              <span className="text-xl Roboto">{t("messages.info4")}</span>
             </button>
           </div>
-          <span className="font-sans">{t("messages.info5")}</span>
+          <span className="Roboto">{t("messages.info5")}</span>
         </div>
       </div>
     </>
